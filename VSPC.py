a413788ccb8a6946b71b3413f3557eb383691f79
BazovicV <bazovicv@gmail.com>
2026-05-18 21:21:33 +0200

    Working on visuals and pressing Enter to calculate

--- VSPC.py
+++ VSPC.py
@@ -1,29 +1,38 @@
+# A very simple calculator written in Python! Used to learn Tk and more of python.
+
 import tkinter as tk
+import numpy as np
+from numpy import sin as sin
+from numpy import cos as cos # Not yet implemented fully but just learning how to rename functions.
+from numpy import tan as tan
 
-prozor = tk.Tk()
-prozor.title("Kalkulator")
-prozor.geometry("400x200")
+window = tk.Tk()
+window.title("Calculator")
+window.geometry("400x200")
 
-user_input = tk.StringVar(prozor)
+user_input = tk.StringVar(window) # A variable that stores what is written in .Entry().
 
-def racunanje():
+def calculate(event=None): # event=None - Needed because .bind() sends pressed key data to function, and it's set to None so it doesn't confuse our .button.
     try:
-        izraz = user_input.get()
-        rezultat = eval(izraz)
-        tekst.config(text="Rezultat: " + str(rezultat))
+        expression = user_input.get() # .get - gets what is currently written in .Entry().
+        result = eval(expression)
+        txt.config(text="Result: " + str(result)) # .config - configures the text.
     except:
-        tekst.config(text="Ovo nije izraz koji se može izračunati.")
+        txt.config(text="Err")
 
-kalkul = tk.Label(prozor, text="Upišite izraz: ")
-kalkul.pack()
+resultWrite = tk.Frame(window, border=50, width=200, height=200, background="red")
+resultWrite.pack(side="left")
 
-napisati = tk.Entry(prozor, textvariable=user_input)
-napisati.pack()
+write = tk.Entry(resultWrite, textvariable=user_input).grid(row=0)
 
-dugme = tk.Button(prozor, text="Izračunaj", command=racunanje)
-dugme.pack() 
+button= tk.Button(window, text="Calculate", command=calculate)
+window.bind("<Return>", calculate) # .bind - binds key to a function.
+button.pack() 
 
-tekst = tk.Label(prozor, text="")
-tekst.pack()
+empty0 = tk.Label(resultWrite, text="").grid(row=1)
+txt = tk.Label(resultWrite, text="")
+txt.grid(row=2)
 
-prozor.mainloop()
+window.mainloop()
+
+# VISUALS NOT DONE, that's why it looks ugly, sorry! At least it works.
